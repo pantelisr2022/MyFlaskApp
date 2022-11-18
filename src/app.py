232db342c6fd377045fc2a400f18b9ffdc08dace
Pantelis R <pantelisr1@outlook.com.gr>
2022-11-18 11:06:39 +0200

    1ο commit

---
+++ src/app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template
+import json
+
+
+app = Flask(__name__)
+
+@app.route("/index/")
+@app.route("/")
+@app.route("/<name>")
+def root(name=None):
+    return render_template("index.html", myname=name)
+
+@app.route("/books")
+def books():
+
+    with open("mybooks.json") as json_file:
+        jdict = json.load(json_file)
+        print(jdict)
+
+    return render_template("books.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
